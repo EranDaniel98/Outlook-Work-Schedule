94b5623dc56080922dcd1e147f03555dcd555e90
Eran <erand1998@gmail.com>
2022-05-24 23:53:18 +0300

    added strings file

---
+++ strings.py
@@ -0,0 +1,6 @@
+morning_monitor_job = 'מנטורינג + מסרים 05:45-15:00'
+morning_LS_WS_job = 'LS + WS 05:45-15:00'
+evening_monitor_job = 'מנטורינג + מסרים 14:45-23:00'
+evening_LS_WS_job = 'LS + WS 14:45-23:00'
+training_job = '08:00-17:00'
+
