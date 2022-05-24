152a0c6e676cbbfa3f8e0650e54245804b79ff99
Eran <erand1998@gmail.com>
2022-05-24 23:52:36 +0300

    Finished parsed work schedule table

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import win32com.client as client
 from bs4 import BeautifulSoup as bs
+import strings
+import itertools
 
 
 outlook = client.Dispatch("Outlook.Application")
 mapi = outlook.GetNamespace("MAPI")
 
-def get_folder_by_name(folder_name):
-    for folder in mapi.Folders.Item(1).Folders: 
+def get_folder_by_name(folder_name, index):
+    for folder in mapi.Folders.Item(index).Folders: 
         if folder.Name == folder_name:
             return folder
     
@@ -19,50 +21,51 @@ def parse_mail(mail, user):
     table_tds = [x.getText() for x in mail_table.find_all('td')]
 
     week_dates = table_tds[1:7]
-    morning_shift_index = table_tds.index('מנטורינג + מסרים 05:45-15:00') + 1
-    morning_shift_index2 = table_tds.index('LS + WS 05:45-15:00') + 1
-    evening_shift_index = table_tds.index('מנטורינג + מסרים 14:45-23:00') + 1
-    training_shift_index = table_tds.index('08:00-17:00')
+    morning_shift_MSGs, morning_shift_LSWS, evening_shift_MSGs, evening_shift_LSWS = get_worker_lists(table_tds)
 
-    morning_shift_workers = table_tds[morning_shift_index:morning_shift_index2 + 6]
-    evening_shift_workers = table_tds[evening_shift_index:training_shift_index]
-    print(week_dates)
-    print(len(morning_shift_workers), morning_shift_workers)
-    #print()
-    #print(len(evening_shift_workers), evening_shift_workers)
+    print_work_days(user, morning_shift_MSGs, morning_shift_LSWS, week_dates)
+    print_work_days(user, evening_shift_MSGs, evening_shift_LSWS, week_dates)
 
+def print_work_days(user, msgs_shift, lsws_shift, week_dates):
 
-    days_i_work = [i for i, s in enumerate(morning_shift_workers) if user in s] # Get the index where Eran shown
-    print(days_i_work)
-    for i in days_i_work:
-        if i >= 0 and i < 6:
-            print(week_dates[i], '05:45-15:00 Monitoring and MSGs')
+    days_i_work_MSGs = [i for i, s in enumerate(msgs_shift) if user in s] # Get the index where Eran shown in MSGs
+    days_i_word_LSWS = [i for i, s in enumerate(lsws_shift) if user in s] # Get the index where Eran shown in LSWS
 
-        if i > 6 and i < len(morning_shift_workers): 
-            print(week_dates[len(morning_shift_workers) - i + 1], '05:45-15:00 LS + WS')
-    
-    days_i_work = [i for i, s in enumerate(evening_shift_workers) if user in s] # Get the index where Eran shown
-    for i in  days_i_work:
-        if i >= 0 and i < 6: 
-            print(week_dates[i], '14:45-23:00 Monitoring and MSGs')
+    for i, j in itertools.zip_longest(days_i_work_MSGs, days_i_word_LSWS):
+        if i != None:
+            print(week_dates[i - 1], strings.morning_monitor_job)
         
-        if i > 6 and i < len(evening_shift_workers): 
-            print(week_dates[i-7], '14:45-23:00 LS + WS')
+        if j != None:
+            print(week_dates[j - 1], strings.morning_LS_WS_job)
 
+def get_worker_lists(table_tds):
+    morning_shift_monitor_index = table_tds.index(strings.morning_monitor_job)
+    morning_shift_LSWS_index = table_tds.index(strings.morning_LS_WS_job)
+    evening_shift_monitor_index = table_tds.index(strings.evening_monitor_job)
+    evening_shift_LSWS_index = table_tds.index(strings.evening_LS_WS_job)
 
+    morning_shift_MSGs = table_tds[morning_shift_monitor_index : morning_shift_LSWS_index]
+    morning_shift_LS = table_tds[morning_shift_LSWS_index : morning_shift_LSWS_index + 7]
+    evening_shift_MSGs = table_tds[evening_shift_monitor_index : evening_shift_LSWS_index]
+    evening_shift_LS = table_tds[evening_shift_LSWS_index : evening_shift_LSWS_index + 7]
+
+    return morning_shift_MSGs, morning_shift_LS, evening_shift_MSGs, evening_shift_LS
+    
+
+    
 
 def main():
-    folder =  get_folder_by_name("Work Schedule")
+    for i in range(1,4):
+        folder =  get_folder_by_name("Work Schedule", i)
 
-    if folder == None:
-        print('ERROR 404: Folder not found')
-        return
-
-    #print(i.SentOn.strftime("%d-%m-%y")) - Date
-    latest_mail = list(folder.Items)[1]
-    #for i in folder.Items:
-    print(latest_mail.subject)
-    parse_mail(latest_mail, 'Eran')
+        if folder != None:
+            latest_mail = list(folder.Items)[0]
+            print('Subject: ', latest_mail.subject)
+            print()
+            parse_mail(latest_mail, 'Eran')
+            return
+        
+    print('ERROR 404: Folder not found')
 
 
 
